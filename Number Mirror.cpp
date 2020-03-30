e1bb2af140e964b46993a5513551f522c93c4f81
Sneha Herle <snehaherle@gmail.com>
2020-03-31 00:05:34 +0530

    Create Number Mirror.cpp

---
+++ Number Mirror.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/START01
+
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t=0;
+    cin>>t;
+    cout<<t;
+    return 0;
+}
